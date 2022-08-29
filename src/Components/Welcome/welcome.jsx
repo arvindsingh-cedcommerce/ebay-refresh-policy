f47c1ffc2d142ca75decaf0911201e951a4e34d2
utkarshsahu-cedcommerce <utkarshsahu@cedcommerce.com>
2022-08-29 16:31:49 +0530

    welcome page routing

--- src/Components/Welcome/welcome.jsx
+++ src/Components/Welcome/welcome.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Page } from "@shopify/polaris";
+import { checkStepCompleted } from "../../Apirequest/registrationApi";
 const welcomeScreen = require("./../../assets/Sell-on-ebay-marketplace-08.jpg");
 
 class Welcome extends Component {
@@ -13,13 +14,16 @@ class Welcome extends Component {
     }, 4000);
   }
 
-  routetoUrl() {
-    if (true) {
-      this.redirect("/registrations");
-    } else {
-      this.redirect("/panel/products");
+  routetoUrl = async () => {
+    let { success, data } = await checkStepCompleted();
+    if (success) {
+      if (data < 4) {
+        this.redirect("/registrations");
+      } else {
+        this.redirect("/panel/ebay/dashboard");
+      }
     }
-  }
+  };
 
   render() {
     return (
