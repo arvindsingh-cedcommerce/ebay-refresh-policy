5ca5a6c0311c07f4962ce69f978314fe0fa11678
utkarshsahu-cedcommerce <utkarshsahu@cedcommerce.com>
2022-09-02 16:57:47 +0530

    update order fixed

--- src/Components/Panel/Marketplaces/Ebay/Orders/Helper/viewOrderHelper.js
+++ src/Components/Panel/Marketplaces/Ebay/Orders/Helper/viewOrderHelper.js
@@ -4,7 +4,9 @@ export const parseDataForSave = (data) => {
     if (typeof data[key] === "object" && Object.keys(data[key]).length > 0) {
       let tempObj = {};
       for (const innerKey in data[key]) {
-        if (data[key][innerKey]) {
+        if (innerKey === "last_name") {
+          tempObj[innerKey] = data[key][innerKey];
+        } else if (data[key][innerKey]) {
           tempObj[innerKey] = data[key][innerKey];
         }
       }
@@ -18,4 +20,44 @@ export const parseDataForSave = (data) => {
     }
   }
   return parsedData;
+};
+
+export const extractUpdateOrderData = (data, updateOrder, setUpdateOrder) => {
+  console.log(data);
+  let extractedData = { ...updateOrder };
+  for (const key in data) {
+    switch (key) {
+      case "email":
+        extractedData[key] = data[key];
+        break;
+      case "phone_number":
+        extractedData[key] = data[key];
+        break;
+      case "shipping_address":
+        extractedData[key]["full_name"] = data[key]["full_name"];
+        extractedData[key]["country"] = data[key]["country"];
+        extractedData[key]["phone_number"] = data[key]["phone_number"];
+        extractedData[key]["company"] = data[key]["company"];
+        extractedData[key]["city"] = data[key]["city"];
+        extractedData[key]["province"] = data[key]["province"];
+        extractedData[key]["zip"] = data[key]["zip"];
+        extractedData[key]["company"] = data[key]["company"];
+        extractedData[key]["address1"] =
+          data[key]["address1"] + " " + data[key]["address1"];
+        break;
+      case "customer":
+        extractedData[key] = Object.keys(data[key]).length > 0;
+        break;
+      case "tags":
+        extractedData[key] = data[key];
+        break;
+      case "note":
+        extractedData[key] = data[key];
+        break;
+      default:
+        break;
+    }
+  }
+  console.log(extractedData);
+  setUpdateOrder(extractedData);
 };
