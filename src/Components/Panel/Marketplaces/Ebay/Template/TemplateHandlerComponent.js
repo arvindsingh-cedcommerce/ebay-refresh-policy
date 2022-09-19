2d0f3c1931123e0f6d66fccd28352106720e0885
utkarshsahu-cedcommerce <utkarshsahu@cedcommerce.com>
2022-09-19 18:11:23 +0530

    actions in edit template

--- src/Components/Panel/Marketplaces/Ebay/Template/TemplateHandlerComponent.js
+++ src/Components/Panel/Marketplaces/Ebay/Template/TemplateHandlerComponent.js
@@ -1,12 +1,26 @@
 import React, { Component } from "react";
 import { parseQueryString } from "../../../../../services/helperFunction";
-import { Page } from "@shopify/polaris";
-import { saveTemplates } from "../../../../../Apirequest/ebayApirequest/templatesApi";
+import {
+  Button,
+  Checkbox,
+  Modal,
+  Page,
+  Select,
+  Stack,
+  TextContainer,
+} from "@shopify/polaris";
+import {
+  duplicateTemplate,
+  saveTemplates,
+  saveTemplateToProfile,
+} from "../../../../../Apirequest/ebayApirequest/templatesApi";
 import { notify } from "../../../../../services/notify";
 import TitleTemplatePolaris from "./TemplateBody/TitleTemplatePolaris";
 import PricingTemplatePolaris from "./TemplateBody/PricingTemplatePolaris";
 import CategoryTemplatePolarisNew from "./TemplateBody/CategoryTemplatePolarisNew";
 import FinalInventoryTemplate from "./TemplateBody/FinalInventoryTemplate";
+import { getProfiles } from "../../../../../APIrequests/ProfilesAPI";
+import { getProfilesURLFilter } from "../../../../../URLs/ProfilesURL";
 
 class TemplateHandlerComponent extends Component {
   constructor(props) {
@@ -17,11 +31,31 @@ class TemplateHandlerComponent extends Component {
       loader: false,
       siteID: "",
       shopID: "",
+      saveTemplateToProfileModal: false,
+      profilesList: [],
+      selectedProfile: "",
     };
   }
 
+  getAllProfiles = async () => {
+    let dataToPost = {
+      marketplace: "ebay",
+      grid: true
+    };
+    let { success, data, message } = await getProfiles(
+      getProfilesURLFilter,
+      dataToPost
+    );
+    if (success && data.rows.length) {
+      let temp = data.rows.map((row) => {
+        return { label: row.name, value: row.profile_id };
+      });
+      this.setState({ profilesList: [...temp] });
+    }
+  };
   componentDidMount() {
     this.getParams();
+    this.getAllProfiles();
   }
 
   async getParams() {
@@ -97,6 +131,63 @@ class TemplateHandlerComponent extends Component {
     }
   }
 
+  createDuplicateTemplate = async () => {
+    const postData = { template_id: this.state.id };
+    let { success, message } = await duplicateTemplate(postData);
+    if (success) {
+      notify.success(message);
+      this.redirect("/panel/ebay/templates");
+    } else {
+      notify.error(message);
+      this.setState({ saveTemplateToProfileModal: false });
+    }
+  };
+
+  saveToAllProfile = async (profileId) => {
+    const postData = {
+      profile_id: profileId,
+      template_id: this.state.id,
+      type: this.state.type,
+    };
+    if (this.state.type === "category") postData["shop_id"] = this.state.shopID;
+    let { success, message } = await saveTemplateToProfile(postData);
+    if (success) {
+      notify.success(message);
+      this.redirect("/panel/ebay/templates");
+    } else {
+      notify.error(message);
+      this.setState({ saveTemplateToProfileModal: false });
+    }
+  };
+
+  saveToSpecificProfile = () => {
+    let temp = { ...this.state };
+    temp["saveTemplateToProfileModal"] = true;
+    this.setState(temp);
+  };
+
+  prepareOptionsforAction = () => {
+    const tempArr = [
+      {
+        content: "Create duplicate template",
+        onAction: this.createDuplicateTemplate,
+      },
+    ];
+    if (this.state.profilesList.length) {
+      tempArr.push(
+        {
+          content: "Save to all profile",
+          onAction: () => this.saveToAllProfile("all"),
+        },
+        {
+          content: "Save to specified profile",
+          onAction: this.saveToSpecificProfile,
+        }
+      );
+    }
+    return tempArr;
+  };
+
   render() {
     let { type, id } = this.state;
     let title = !id ? "Create template" : "Edit template";
@@ -110,8 +201,74 @@ class TemplateHandlerComponent extends Component {
         ]}
         title={title}
         fullWidth={true}
+        actionGroups={
+          id
+            ? [
+                {
+                  title: "Actions",
+                  actions: this.prepareOptionsforAction(),
+                },
+              ]
+            : []
+        }
       >
         {this.getTemplatebody(type)}
+        <Modal
+          open={this.state.saveTemplateToProfileModal}
+          onClose={() => {
+            let temp = { ...this.state };
+            temp["saveTemplateToProfileModal"] = false;
+            this.setState(temp);
+          }}
+          title="Please select one profile for save template"
+        >
+          <Modal.Section>
+            <TextContainer>
+              <Stack distribution="fillEvenly" spacing="extraTight">
+                {/* {this.state.profilesList.map((profile, index) => (
+                  <Checkbox
+                    label={profile.name}
+                    checked={profile.checked}
+                    onChange={() => {
+                      let temp = { ...this.state };
+                      temp["profilesList"][index]["checked"] = !profile.checked;
+                      this.setState(temp);
+                    }}
+                  />
+                ))} */}
+                <Select
+                  options={this.state.profilesList}
+                  value={this.state.selectedProfile}
+                  onChange={(e) => {
+                    this.setState({ selectedProfile: e });
+                  }}
+                  placeholder="Please Select..."
+                />
+              </Stack>
+              <Stack distribution="center">
+                <Button
+                  onClick={() => {
+                    let temp = { ...this.state };
+                    temp["selectedProfile"] = "";
+                    temp["saveTemplateToProfileModal"] = false;
+                    this.setState(temp);
+                  }}
+                >
+                  Cancel
+                </Button>
+                <Button
+                  primary
+                  onClick={() => {
+                    this.saveToAllProfile(this.state.selectedProfile);
+                  }}
+                  disabled={!this.state.selectedProfile}
+                >
+                  Save
+                </Button>
+              </Stack>
+            </TextContainer>
+          </Modal.Section>
+        </Modal>
       </Page>
     );
   }
