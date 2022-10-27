f217a0115fdf1d2c2b676c6e7e8ad63dfa9315a8
RishiDevGit <rishirajsaxena@makewebbetter.com>
2022-10-27 13:08:13 +0530

    rishi-feature dangerously set inner html

--- src/Components/Panel/Marketplaces/Ebay/Help/GroupFAQComponent.js
+++ src/Components/Panel/Marketplaces/Ebay/Help/GroupFAQComponent.js
@@ -14,62 +14,66 @@ import TabsComponent from "../../../../AntDesignComponents/TabsComponent";
 const GroupFAQComponent = ({ faqs, setSearchFaqArray }) => {
   const getTabStructure = () => {
     let returnStructure = {};
-    faqs.forEach((faq,i) => {
-      if(faq["qas"] && faq["qas"].length>0) {
-      returnStructure[faq["category"]] =faq["qas"]
-      .map((faqKey,index) => (
-        <>
-          <div
-            style={{ cursor: "pointer" }}
-            onClick={() => {
-              let temp = [...faqs ];
-              temp[i]["qas"][index]["isOpen"] =
-                !faq["qas"][index]["isOpen"];
+    faqs.forEach((faq, i) => {
+      if (faq["qas"] && faq["qas"].length > 0) {
+        returnStructure[faq["category"]] = faq["qas"].map((faqKey, index) => (
+          <>
+            <div
+              style={{ cursor: "pointer" }}
+              onClick={() => {
+                let temp = [...faqs];
+                temp[i]["qas"][index]["isOpen"] = !faq["qas"][index]["isOpen"];
                 setSearchFaqArray(temp);
-            }}
-            ariaExpanded={faq["qas"][index]["isOpen"]}
-          >
-            <Card>
-              <Card.Section>
-                <Stack distribution={"equalSpacing"}>
-                  <b>{faqKey["question"]}</b>
-                  <Button
-                    plain
-                    icon={
-                      true ? (
-                        <Icon source={ChevronDownMinor} />
-                      ) : (
-                        <Icon source={ChevronRightMinor} />
-                      )
-                    }
-                  />
-                </Stack>
-              </Card.Section>
-              <Collapsible
-                transition={{
-                  duration: "500ms",
-                  timingFunction: "ease-in-out",
-                }}
-                open={faq["qas"][index]["isOpen"]}
-              >
-                <Card>
-                  <Card.Section>
-                    {faq["qas"][index]["value"]}
-                  </Card.Section>
-                </Card>
-              </Collapsible>
-            </Card>
-          </div>
-          <br />
-        </>
-      ));
-    }  });
-  
+              }}
+              ariaExpanded={faq["qas"][index]["isOpen"]}
+            >
+              <Card>
+                <Card.Section>
+                  <Stack distribution={"equalSpacing"}>
+                    <b>{faqKey["question"]}</b>
+                    <Button
+                      plain
+                      icon={
+                        true ? (
+                          <Icon source={ChevronDownMinor} />
+                        ) : (
+                          <Icon source={ChevronRightMinor} />
+                        )
+                      }
+                    />
+                  </Stack>
+                </Card.Section>
+                <Collapsible
+                  transition={{
+                    duration: "500ms",
+                    timingFunction: "ease-in-out",
+                  }}
+                  open={faq["qas"][index]["isOpen"]}
+                >
+                  <Card>
+                    <Card.Section>
+                      <div
+                        dangerouslySetInnerHTML={{
+                          __html: faq["qas"][index]["value"],
+                        }}
+                      />
+                      {/* {faq["qas"][index]["value"]} */}
+                    </Card.Section>
+                  </Card>
+                </Collapsible>
+              </Card>
+            </div>
+            <br />
+          </>
+        ));
+      }
+    });
+
     return returnStructure;
   };
   return (
     <TabsComponent
-      tabPosition={window.innerWidth<=768?"top":"left"}
+      tabPosition={window.innerWidth <= 768 ? "top" : "left"}
       totalTabs={faqs.length}
       tabContents={getTabStructure()}
     />
