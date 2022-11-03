96defb7b42108b9ddda14bba3db3213a8fed866a
utkarshsahu-cedcommerce <utkarshsahu@cedcommerce.com>
2022-11-03 12:16:40 +0530

    token file

---
+++ src/HelperVariables.js
@@ -0,0 +1,8 @@
+export const tokenExpireValues = [
+  "token_expired",
+  "token_decode_error",
+  "future_token",
+  "unable_to_remove_the_token",
+  "missing_token",
+  "token_user_not_found",
+];
